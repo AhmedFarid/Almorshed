13f89459245eb896699197f26495d68d02e56586
farido <farido@faridos-Mac.local>
2019-01-22 02:34:53 -0800

    Initial Commit

---
+++ Almorshed/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Almorshed
+//
+//  Created by farido on 1/22/19.
+//  Copyright © 2019 Bakers. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
